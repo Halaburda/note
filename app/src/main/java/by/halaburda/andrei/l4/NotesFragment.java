fcb8e2dcb65cf9d1c347ba9d29a96cd9a0ab347d
novab <Novableach@mail.ru>
2018-12-06 20:10:22 +0300

    add item_note

--- app/src/main/java/by/halaburda/andrei/l4/NotesFragment.java
+++ app/src/main/java/by/halaburda/andrei/l4/NotesFragment.java
@@ -4,71 +4,75 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import java.util.ArrayList;
-
 
 
 public class NotesFragment extends Fragment {
 
     private int statusType = Constants.STATUS_DEFAULT;
-    private String strNote;
-    private TextView tvNotes;
+
+    ArrayList<Note> notes;
+    RecyclerView rvNotes;
+
+//    private TextView tvNotes;
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_for_notes, container, false);
-        tvNotes = v.findViewById(R.id.tvNotes);
-        switch (statusType) {
-            case Constants.STATUS_CURRENT:
-                loadCurrentNotes();
-                break;
-            case Constants.STATUS_EXECUTEO:
-                loadExecutedNotes();
-                break;
-            default:
 
-        }
-        tvNotes.setText(strNote);
+        loadNotes();
+
+        initRCV(v);
+
+//        switch (statusType) {
+//            case Constants.STATUS_CURRENT:
+//                loadCurrentNotes();
+//                break;
+//            case Constants.STATUS_EXECUTEO:
+//                loadExecutedNotes();
+//                break;
+//            default:
+//
+//        }
+
 
         return v;
     }
 
-    private void loadExecutedNotes() {
-        final ArrayList<Note> notes = ListNote.getInstance().getNotes();
-        for (Note note : notes) {
-            int status = note.getStatus();
-            if (status != Constants.STATUS_CURRENT) {
-                continue;
-            }
-            String title = note.getTitle();
-            String description = note.getDescription();
-            strNote += (title + " " + description + "\n");
+    private void initRCV(View v) {
 
-        }
-    }
+        rvNotes = v.findViewById(R.id.rvNotes);
 
-    private void loadCurrentNotes() {
-
-        final ArrayList<Note> notes = ListNote.getInstance().getNotes();
-        for (Note note : notes) {
-            int status = note.getStatus();
-            if (status != Constants.STATUS_EXECUTEO) {
-                continue;
-            }
-            String title = note.getTitle();
-            String description = note.getDescription();
-            strNote += (title + " " + description + "\n");
-
-        }
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        rvNotes.setLayoutManager(llm);
 
     }
+
+    private void loadNotes() {
+
+        notes = ListNote.getInstance().getNotes(statusType);
+
+    }
+
+//    private void loadExecutedNotes() {
+//        final ArrayList<Note> notes = ListNote.getInstance().getNotes(statusType);
+//
+//    }
+//
+//    private void loadCurrentNotes() {
+//
+//        final ArrayList<Note> notes = ListNote.getInstance().getNotes(statusType);
+//
+//
+//    }
 
     public int getStatusType() {
         return statusType;
